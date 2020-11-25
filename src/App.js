6a15659ce4c889476381535c5f2772c456029aa8
Bálint Áts <atsbalint@gmail.com>
2020-11-25 15:52:18 +0100

    init commit

--- src/App.js
+++ src/App.js
@@ -1,22 +1,32 @@
 import logo from './logo.svg';
 import './App.css';
+import 'firebase/firestore';
+import 'firebase/auth';
+
+import { useAuthState } from 'react-firebase-hooks/auth';
+import { useCollectionData } from 'react-firebase-hooks/firestore';
+
+firebase.initializeApp({
+  apiKey: "AIzaSyBb1TX6Lozb9ipkX4Cbvl9JHEnW3M8RLU8",
+  authDomain: "csetrum-13250.firebaseapp.com",
+  databaseURL: "https://csetrum-13250.firebaseio.com",
+  projectId: "csetrum-13250",
+  storageBucket: "csetrum-13250.appspot.com",
+  messagingSenderId: "411645516223",
+  appId: "1:411645516223:web:5b0fc6fa3ba3941929a90a",
+  measurementId: "G-EK810KDR84"
+
+});
+
+// references
+const auth = firebase.auth();
+const firestore = firebase.firestore();
+
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
       </header>
     </div>
   );
