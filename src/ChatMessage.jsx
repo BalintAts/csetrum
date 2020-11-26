dcc65a651fe9727b09e0243539902959b6c2cc5b
Bálint Áts <atsbalint@gmail.com>
2020-11-26 14:48:08 +0100

    Add style to  ChatMessage, text align

--- src/ChatMessage.jsx
+++ src/ChatMessage.jsx
@@ -1,10 +1,20 @@
 import React from 'react'
+import { auth } from './App';
+import './styles/ChatMessageStyle.css';
 
 export const ChatMessage = (props) => {
-    const { text, uid } = props.message;
+    const { text, uid, photoURL } = props.message;
+
+    //ternary magic, string according to codition
+    const sendRecieved = uid === auth.currentUser.userId ? "sent" : "received";
 
     return (
-        <p>{text}</p>
+        // <div className={{ sendRecieved }}>
+        <div className="sent">
+
+            <img src={photoURL} />
+            <p>{text}</p>
+        </div>
     )
 }
 
